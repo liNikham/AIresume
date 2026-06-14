bff7bd5f229263f6ea0672a91bb808a8a819c2ca
liNikham <mahadiknikhil2508@gmail.com>
2026-06-14 10:46:21 +0530

    using weasyprint instead of playwright

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,25 +1,12 @@
-from playwright.sync_api import sync_playwright
+from weasyprint import HTML
 
 
 def html_to_pdf(
         html_file,
         output_pdf):
 
-    with sync_playwright() as p:
-
-        browser = p.chromium.launch()
-
-        page = browser.new_page()
-
-        page.goto(
-            f"file:///{html_file}",
-            wait_until="networkidle"
-        )
-
-        page.pdf(
-            path=output_pdf,
-            format="A4",
-            print_background=True
-        )
-
-        browser.close()
+    HTML(
+        filename=html_file
+    ).write_pdf(
+        output_pdf
+    )
